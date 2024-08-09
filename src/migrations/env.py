262209aa769848bf4f726b7dd0788a5906b3f75f
deltaVII <said.myrzakov@gmail.com>
2024-08-09 09:43:39 +0400

    fix migrations and database

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -1,22 +1,28 @@
-import asyncio
 import os
 from logging.config import fileConfig
 
 from alembic import context
-from alembic.script import ScriptDirectory
 from sqlalchemy import pool
-from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy import engine_from_config
 
 from database.models import *  # noqa: F403
 from database.models.base import AlchemyBaseModel
 
+from dotenv import load_dotenv
+
+load_dotenv() # мне похуй на этот говнокод
+
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "POSTGRES_HOST", os.environ["POSTGRES_HOST"])
+print(os.environ["IS_DOCKER"])
+if os.environ["IS_DOCKER"] == "1":
+    config.set_section_option(section, "POSTGRES_HOST", os.environ["POSTGRES_HOST"])
+else:
+    config.set_section_option(section, "POSTGRES_HOST", "127.0.0.1")
+
 config.set_section_option(
-    section, "POSTGRES_HOST_PORT", os.environ["POSTGRES_HOST_PORT"]
+    section, "POSTGRES_PORT", os.environ["POSTGRES_PORT"]
 )
 config.set_section_option(section, "POSTGRES_DB", os.environ["POSTGRES_DB"])
 config.set_section_option(section, "POSTGRES_USER", os.environ["POSTGRES_USER"])
@@ -28,61 +34,50 @@ if config.config_file_name is not None:
 target_metadata = AlchemyBaseModel.metadata
 
 
-def process_revision_directives(context, revision, directives):
-    migration_script = directives[0]
-    head_revision = ScriptDirectory.from_config(context.config).get_current_head()
-
-    if head_revision is None:
-        new_rev_id = 1
-    else:
-        last_rev_id = int(head_revision.lstrip("0"))
-        new_rev_id = last_rev_id + 1
-    migration_script.rev_id = f"{new_rev_id:04}"
-
-
 def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
 
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        process_revision_directives=process_revision_directives,
     )
 
     with context.begin_transaction():
         context.run_migrations()
 
 
-def do_run_migrations(connection: Connection) -> None:
-    context.configure(
-        connection=connection,
-        target_metadata=target_metadata,
-        process_revision_directives=process_revision_directives,
-    )
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
 
-    with context.begin_transaction():
-        context.run_migrations()
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
 
-
-async def run_async_migrations() -> None:
-
-    connectable = async_engine_from_config(
+    """
+    connectable = engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
 
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
-    await connectable.dispose()
-
-
-def run_migrations_online() -> None:
-
-    asyncio.run(run_async_migrations())
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 if context.is_offline_mode():
