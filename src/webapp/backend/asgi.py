beae62087c85e5f210bfcb229f19baae2fc1fd39
deltaVII <said.myrzakov@gmail.com>
2024-08-19 13:31:41 +0400

    update interface

--- src/webapp/backend/asgi.py
+++ src/webapp/backend/asgi.py
@@ -10,8 +10,7 @@ from backend.dependencies import get_current_user, CurrendUser
 app = FastAPI()
 
 """
-тут не решен вопрос с откатом просмотренных анкет, 
-создать поле история?
++ уведомления через сокеты
 """
 
 
@@ -21,7 +20,16 @@ async def register_user(
         user_image: UploadFile, 
         current_user: Annotated[CurrendUser, get_current_user]
     ) -> 200:
-    ...
+    """Регистрация фронта
+
+    Args:
+        user_data (RegisterUserScheme): _description_
+        user_image (UploadFile): _description_
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        200: _description_
+    """
 
 
 @app.put("user")
@@ -29,7 +37,15 @@ async def update_user_data(
         user_data: UpdateUserScheme,
         current_user: Annotated[CurrendUser, get_current_user]
     ) -> 200:
-    ...
+    """Обновление полей юзера (без фотографии)
+
+    Args:
+        user_data (UpdateUserScheme): _description_
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        200: _description_
+    """
 
 
 @app.put("user_image")
@@ -37,14 +53,31 @@ async def update_user_image(
         user_image: UploadFile, 
         current_user: Annotated[CurrendUser, get_current_user]
     ) -> 200:
-    ...
+    """Обновление фотографии юзера
+
+    Args:
+        user_image (UploadFile): _description_
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        200: _description_
+    """
+    
 
 
 @app.get("/profile_for_dating")
 async def get_profile_for_dating(
         current_user: Annotated[CurrendUser, get_current_user]
     ) -> ProfileForDatingScheme:
-    ...
+    """Возвращает случайный профиль для знакомства
+    подходящий под критерии
+
+    Args:
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        ProfileForDatingScheme: _description_
+    """
 
 
 @app.post("/reaction_to_profile") # если прислал, значит одобрил
@@ -52,19 +85,43 @@ async def reaction_to_profile(
         profile_id: int,
         current_user: Annotated[CurrendUser, get_current_user],
     ) -> 200:
-    ...
+    """Означает лайк на профиль
+    + уведомления через сокеты
+
+    Args:
+        profile_id (int): _description_
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        200: _description_
+    """
 
 
 @app.get("/profiles_who_like_current_user") # патамушта
 async def get_profiles_who_like_current_user(
         current_user: Annotated[CurrendUser, get_current_user]
     ) -> list[ProfileForDatingScheme]:
-    ...
+    """Возвращает профили которые лайкнули текущего пользователя
+
+    Args:
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+
+    Returns:
+        list[ProfileForDatingScheme]: _description_
+    """
 
 
-@app.post("/reaction_to_reaction") # если прислал, значит одобрил
+@app.post("/reaction_to_like")
 async def reaction_to_reaction(
+        reaction: str, # approved/not approved
         profile_id: int,
         current_user: Annotated[CurrendUser, get_current_user],
     ): # я хер знает что возвращать, профиль или создать для этого отдельный метод
-    ...
+    """Реакция на лайк, 
+    нужно знать реакцию чтобы убрать первую реакцию из уведомлений
+    + уведомления через сокеты
+
+    Args:
+        profile_id (int): _description_
+        current_user (Annotated[CurrendUser, get_current_user]): _description_
+    """
--- src/webapp/backend/dependencies.py
+++ src/webapp/backend/dependencies.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 
 
 class CurrendUser(BaseModel):
+    id: int
     tg_username: str
     name: str
 
